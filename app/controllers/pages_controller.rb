a238b0e8112163ba6cbf2926c1288697c8b0d003
tryrush <support@tryrush.com>
2017-10-19 04:34:14 -0400

    More components/pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,16 @@ class PagesController < ApplicationController
   def home
   end
 
+  def fencing
+  end
+
   def state
   end
 
   def contractor
+  end
+
+  def advice
   end
   
   def faq
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  get '/fencing' => 'pages#fencing'
+
   get '/states' => 'pages#state'
   get '/contractors' => 'pages#contractor'
+  get '/home-advice' => 'pages#advice'
 
   get '/faq' => 'pages#faq'
 
