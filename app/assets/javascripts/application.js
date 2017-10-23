c3a6f810f343797ae498b4e3b81090f76efd936d
Konstantin Kozhin <konstantin@codedred.com>
2017-10-24 02:47:42 +0300

    Assets merged

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require leaflet
+//= require leaflet-draw
 //= require jquery3
-//= require popper
-//= require bootstrap
 //= require jquery_ujs
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
-
-
